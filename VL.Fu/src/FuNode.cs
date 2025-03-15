c7014a82f6b9a97268515570b6486b05384229a6
antokhio <antokhio@gmail.com>
2025-03-15 11:53:29 +0300

    init test fu node

---
+++ VL.Fu/src/FuNode.cs
@@ -0,0 +1,30 @@
+﻿using Stride.Core.Mathematics;
+using VL.Core.Import;
+using VL.Lib.Collections;
+
+namespace Fu;
+
+[ProcessNode()]
+public class FuNode
+{
+    private RectangleF _layout;
+    public void SetLayout(RectangleF layout)
+    {
+        if (_layout != layout)
+        {
+            _layout = layout;
+        }
+    }
+
+    private Spread<FuNode?>? _children;
+    public void SetChildren(Spread<FuNode?>? children)
+    {
+        if (_children != children)
+        {
+            _children = children;
+        }
+    }
+
+
+
+}
