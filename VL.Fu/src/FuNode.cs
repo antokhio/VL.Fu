01d6d86fbea735d2b6a8f4891ca9639c6bcd57c3
antokhio <antokhio@gmail.com>
2025-11-24 12:44:27 +0300

    added context consumer

---
+++ VL.Fu/src/FuNode.cs
@@ -0,0 +1,14 @@
+﻿using VL.Core;
+using VL.Core.Import;
+using VL.Fu.Core;
+
+namespace VL.Fu
+{
+    [ProcessNode(HasStateOutput = true, FragmentSelection = FragmentSelection.Explicit)]
+    public class FuNode : ContextConsumer
+    {
+        [Fragment]
+        public FuNode(NodeContext nodeContext)
+            : base(nodeContext) { }
+    }
+}
